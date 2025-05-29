1033e3685591b5e94bbb99d33f9be0af870aec03
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-05-29 13:54:08 +0700

    half header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 // import { useState, useEffect } from "react";
 import "../src/App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="bg-gray-800 min-h-screen flex items-center justify-center">
         <div className="bg-[#1A1A1A] w-full sm:max-w-[360px] h-[100vh] md:h-[830px]">
-          <div className="p-4 text-white">
-            <h1 className="text-3xl font-maven-pro">Start</h1>
-            <p className="mt-2 font-maven-pro">
-              Это квадрат 360x830px на десктопе и на всю ширину экрана на
-              мобильных устройствах
-            </p>
+          <div className="p-4">
+            <Header />
           </div>
         </div>
       </div>
